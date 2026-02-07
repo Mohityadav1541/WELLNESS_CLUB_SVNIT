d9ab61ff37bbd6e04b4e1c4dfae1ca39edd569f0
Mohit <mr.mohit1540@gmail.com>
2026-02-07 21:23:29 +0530

    fix(layout): Restore Navbar to App.tsx and re-add SVNIT logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
 import AboutPage from "./pages/AboutPage";
@@ -22,6 +23,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
+        <Navbar />
         <Routes>
           {/* Public Routes */}
           <Route path="/" element={<Home />} />
